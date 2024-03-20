b5c7d8c045306913fe30c6ee6c5fea7734b96c0e
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2024-03-20 23:29:30 +1100

    modify documentation

--- src/main/java/com/nom/graalnom/runtime/nodes/controlflow/NomIfNode.java
+++ src/main/java/com/nom/graalnom/runtime/nodes/controlflow/NomIfNode.java
@@ -1,43 +1,3 @@
-/*
- * Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * The Universal Permissive License (UPL), Version 1.0
- *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
- *
- * (a) the Software, and
- *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package com.nom.graalnom.runtime.nodes.controlflow;
 
 import com.nom.graalnom.runtime.nodes.NomStatementNode;
@@ -57,19 +17,19 @@ public final class NomIfNode extends NomStatementNode {
      * {@link #evaluateCondition executing the condition} can lead to a type error.
      */
     @Node.Child
-    private NomExpressionNode conditionNode;
+    private final NomExpressionNode conditionNode;
 
     /**
      * Statement (or {@link NomBlockNode block}) executed when the condition is true.
      */
     @Node.Child
-    private NomBlockNode thenPartNode;
+    private final NomBlockNode thenPartNode;
 
     /**
      * Statement (or {@link NomBlockNode block}) executed when the condition is false.
      */
     @Node.Child
-    private NomBlockNode elsePartNode;
+    private final NomBlockNode elsePartNode;
 
     /**
      * Profiling information, collected by the interpreter, capturing the profiling information of
